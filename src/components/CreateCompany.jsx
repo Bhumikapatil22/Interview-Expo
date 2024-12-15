69a918fd01053e37eb083d53793de8dd70d0629b
Bhumika Patil <148256877+Bhumikapatil22@users.noreply.github.com>
2024-12-15 18:32:37 +0530

    Update CreateCompany.jsx

--- src/components/CreateCompany.jsx
+++ src/components/CreateCompany.jsx
@@ -42,7 +42,7 @@ function CreateCompany() {
     console.log(formData);
 
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/company/create`, formData);
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/company/create`, formData);
       // alert(response.data.message);
        navigate(`/get-company`);
     } catch (error) {
