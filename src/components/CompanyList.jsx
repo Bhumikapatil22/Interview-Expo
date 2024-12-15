9367235dc73134e34fb67cc31533e7bbff3de542
Bhumika Patil <148256877+Bhumikapatil22@users.noreply.github.com>
2024-12-15 17:59:32 +0530

    Update CompanyList.jsx

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -14,6 +14,7 @@ const CompanyList = () => {
     const fetchCompanies = async () => {
       try {
         const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/company/get`);
+        console.log(response.data);
         if (response.data?.companies) {
           setCompanies(response.data.companies);
         } else {
